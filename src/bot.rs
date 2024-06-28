b0c736818fb36ca9d3055039d3f99cf6c80981e2
farneser <farneser1@gmail.com>
2024-06-29 00:59:16 +0300

    chore: async bot download file

--- src/bot.rs
+++ src/bot.rs
@@ -1,9 +1,11 @@
 use std::process;
+use std::sync::Arc;
 use uuid::Uuid;
 use teloxide::{Bot, prelude::*};
 use teloxide::net::Download;
 use reqwest::Url;
-use tokio::fs;
+use teloxide::types::File;
+use tokio::{fs, spawn};
 use crate::utils;
 
 pub fn get_bot() -> Option<Bot> {
@@ -35,59 +37,85 @@ pub fn get_bot() -> Option<Bot> {
     Some(bot)
 }
 
-pub async fn process_message(bot: Bot, msg: Message) -> Result<(), Box<dyn std::error::Error>> {
-    if let Some(document) = msg.document() {
-        handle_file(&bot, &msg, document.file.id.clone(), document.file_name.as_ref()).await?;
-    } else if let Some(photo) = msg.photo().and_then(|p| p.last()) {
-        handle_file(&bot, &msg, photo.file.id.clone(), None).await?;
-    } else if let Some(video) = msg.video() {
-        handle_file(&bot, &msg, video.file.id.clone(), video.file_name.as_ref()).await?;
-    } else if let Some(animation) = msg.animation() {
-        handle_file(&bot, &msg, animation.file.id.clone(), animation.file_name.as_ref()).await?;
+pub async fn process_message(bot: Arc<Bot>, msg: Message) -> Result<(), Box<dyn std::error::Error>> {
+    let msg_copy = Arc::new(msg);
+
+    if let Some(document) = msg_copy.clone().document() {
+        handle_file(bot.clone(), msg_copy, document.file.id.clone(), document.clone().file_name).await?;
+    } else if let Some(photo) = msg_copy.clone().photo().and_then(|p| p.last()) {
+        handle_file(bot.clone(), msg_copy.clone(), photo.file.id.clone(), None).await?;
+    } else if let Some(video) = msg_copy.clone().video() {
+        handle_file(bot.clone(), msg_copy.clone(), video.file.id.clone(), video.clone().file_name).await?;
+    } else if let Some(animation) = msg_copy.clone().animation() {
+        handle_file(bot.clone(), msg_copy.clone(), animation.file.id.clone(), animation.clone().file_name).await?;
     }
 
     Ok(())
 }
+
 async fn handle_file(
-    bot: &Bot,
-    msg: &Message,
+    bot: Arc<Bot>,
+    msg: Arc<Message>,
     file_id: String,
-    file_name: Option<&String>,
+    file_name: Option<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     bot.send_message(msg.chat.id, "Starting file downloading").await?;
 
     println!("{file_id}");
 
-    let file_info = bot.get_file(file_id).await.unwrap();
-
-    let uuid = Uuid::new_v4();
-
-    let final_file_name = match file_name {
-        Some(name) => format!("files/{}_{}", uuid, name),
-        None => format!("files/{}_{}", uuid, utils::get_file_name_from_path(&file_info.path).unwrap()),
-    };
-
-    println!("{}", file_info.path);
-
     utils::create_directory("files").await.expect("Failed to create directory 'files'");
 
-    let mut dst = fs::File::create(final_file_name.clone()).await?;
+    let bot_clone = Arc::clone(&bot);
 
-    bot.download_file(&utils::get_folder_and_file_name(&file_info.path).unwrap(), &mut dst).await?;
+    let msg_clone_for_spawn = Arc::clone(&msg);
 
-    let final_size = utils::get_file_size(&final_file_name).await?;
+    spawn(async move {
+        let file_info = bot_clone.clone().get_file(file_id).await.unwrap();
 
-    println!("File saved: {:?}", final_file_name);
+        let uuid = Uuid::new_v4();
 
-    bot.send_message(
-        msg.chat.id,
-        format!(
-            "Downloaded. Size: {} bytes\n{}{}",
-            final_size.to_string(),
-            utils::fetch_domain(),
-            final_file_name
-        ),
-    ).await?;
+        let final_file_name = match file_name {
+            Some(name) => format!("files/{}_{}", uuid, name),
+            None => format!("files/{}_{}", uuid, utils::get_file_name_from_path(&file_info.path).unwrap()),
+        };
+
+        println!("{}", &file_info.path);
+
+        if let Err(e) = download_file(&bot_clone, msg_clone_for_spawn.clone(), final_file_name, file_info.clone()).await {
+            eprintln!("Failed to download file: {}", e);
+        }
+    });
 
     Ok(())
+}
+
+async fn download_file(bot: &Bot, msg: Arc<Message>, final_file_name: String, file_info: File) -> Result<String, String> {
+    match fs::File::create(&final_file_name).await {
+        Ok(mut dst) => {
+            if let Ok(_) = bot.download_file(&utils::get_folder_and_file_name(&file_info.path).unwrap(), &mut dst).await {
+                let final_size = utils::get_file_size(&final_file_name).await.unwrap_or(0);
+
+                println!("File saved: {:?}", final_file_name);
+
+                bot.send_message(
+                    msg.chat.id,
+                    format!(
+                        "Downloaded. Size: {} bytes\n\n{}{}",
+                        final_size,
+                        utils::fetch_domain(),
+                        final_file_name
+                    ),
+                ).await.expect("Failed to send message");
+
+                Ok("File downloaded".to_owned())
+            } else {
+                println!("Failed to download the file.");
+                Err("Failed to download the file".to_owned())
+            }
+        }
+        Err(e) => {
+            println!("Failed to create file: {:?}", e);
+            Err("Failed to create file".to_owned())
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod bot;
 mod server;
 mod utils;
 
+use std::sync::Arc;
+use teloxide::Bot;
+use teloxide::prelude::Message;
 use tokio::net::TcpListener;
+use tokio::spawn;
 
 #[tokio::main]
 async fn main() {
@@ -12,21 +16,21 @@ async fn main() {
     let server_port = utils::fetch_server_port();
 
     let bot = bot::get_bot().unwrap();
+    let bot_arc = Arc::new(bot.clone());
 
-    let bot_task = tokio::spawn(async move {
-        teloxide::repl(bot, |bot: teloxide::Bot, msg: teloxide::types::Message| async move {
-            bot::process_message(bot, msg).await.expect("TODO: panic message");
+    let bot_task = spawn(async move {
+        teloxide::repl(bot_arc, |bot: Bot, msg: Message| async move {
+            bot::process_message(Arc::new(bot), msg).await.expect("TODO: panic message");
             Ok(())
         })
             .await;
     });
 
-    let server_task = tokio::spawn(async move {
+    let server_task = spawn(async move {
         let app = server::create_app();
 
-        let addr: String = format!("0.0.0.0:{server_port}").parse().unwrap();
-
-        let listener = TcpListener::bind(addr).await.unwrap();
+        let addr: String = format!("0.0.0.0:{}", server_port);
+        let listener = TcpListener::bind(&addr).await.unwrap();
 
         axum::serve(listener, app).await.unwrap();
     });
